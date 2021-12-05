8b9ae432523258b22df1cf213a4cdc0668225bde
Seleznev1804 <79448643+Seleznev1804@users.noreply.github.com>
2021-12-06 00:12:37 +0300

    Route change

--- application/src/App.js
+++ application/src/App.js
@@ -6,7 +6,7 @@ import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:3006/graphql',
+  uri: 'http://localhost:3000/graphql',
 });
 
 class App extends Component {
